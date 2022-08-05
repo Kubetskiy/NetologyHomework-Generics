5b977b5c3be3d1617dd941c905635f45dbd561cc
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-08-05 16:32:47 +0300

    Change random to class field, init random in constructor

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -4,13 +4,16 @@ import java.util.Random;
 public class MagicBox<T> {
     private final T[] items;
     private int itemCount = 0;
+    private final Random random;
 
     public MagicBox(int numberOfItems) {
         this.items = (T[]) new Object[numberOfItems];
+        random = new Random();
     }
 
     public MagicBox(Class<T[]> cls, int numberOfItems) {
         this.items = cls.cast(Array.newInstance(cls.getComponentType(), numberOfItems));
+        random = new Random();
     }
 
     public boolean add(T item) {
@@ -25,7 +28,6 @@ public class MagicBox<T> {
         if (itemCount < items.length) {
             throw new RuntimeException(String.format("Коробочка не полна(, свободно слотов: %3d", items.length - itemCount));
         }
-        var random = new Random();
         return items[random.nextInt(itemCount - 1)];
     }
 }
